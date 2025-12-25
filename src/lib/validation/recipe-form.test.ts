32ac7311125f3943805b21e5a03c9417a1ac291e
Jakub Drewniak <drewniak.jakub@gmail.com>
2025-12-25 13:06:53 +0100

    ci: tests fails check

--- src/lib/validation/recipe-form.test.ts
+++ src/lib/validation/recipe-form.test.ts
@@ -83,6 +83,7 @@ describe('Recipe Form Validation', () => {
 
         // Assert
         expect(error).toBe('Title must be at least 3 characters')
+        expect(error).toBe('NON EXISTING')
       })
 
       it('should reject title with more than 100 characters', () => {
