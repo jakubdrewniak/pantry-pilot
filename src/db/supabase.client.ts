d54dd67165d283724d24b367c437ebdb908806c6
Jakub Drewniak <drewniak.jakub@gmail.com>
2026-02-27 22:23:01 +0100

    fix: remove redundant mocked value

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -17,9 +17,6 @@ import { createBrowserClient } from '@supabase/ssr'
 
 import type { Database } from './database.types'
 
-// TODO: Remove after households are implemented
-export const DEFAULT_USER_ID = '7d2cf57a-a623-447b-9b8c-c9c764a958d5'
-
 export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
